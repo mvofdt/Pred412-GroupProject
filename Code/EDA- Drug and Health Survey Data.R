9a5d3535dfc496d13a26cc6175547161889d103b
Matthew Kent <mtakent@gmail.com>
2014-11-11 08:18:33 -0500

    Remove old EDA code

--- Code/EDA- Drug and Health Survey Data.R
+++ Code/EDA- Drug and Health Survey Data.R
@@ -1,12 +1,9 @@
 # EDA- Drug and Health Survey Data 
-# 10/21/14
+# 11/11/14
 # Pred 412
 
 # Clear the environment
 rm(list = ls())
-
-# Load the necessary libraries
-library(ggplot2)
 
 # Set the working directory 
 # Note: This will need to change with each user. I'm not sure if we can set this
@@ -14,47 +11,10 @@ library(ggplot2)
 setwd("/Users/kentm/Desktop/_NW MSPA/6- Fall 2014/Pred412-GroupProject")
 
 # Load the data set
-load("Drug and Health Survey Data- 2012.RDA")
+load("Data/Final Data- Drug and Health Survey Data.RData")
 
-# Rename the dataframe
-surveydf <- da34933.0001
-rm(da34933.0001)
 
-# Understanding what variables are available and seem important
-names(surveydf)
 
-# Variables to identify usage
-names(surveydf)[grep("EVER", names(surveydf))]
-
-# Variables to identify age of first trying
-names(surveydf)[grep("TRY", names(surveydf))]
-
-# Respondent gender
-table(surveydf$IRSEX)
-
-# Respondent race
-table(surveydf$NEWRACE2)
-
-# Respondent age
-str(surveydf$AGE2) # Raw age
-table(surveydf$AGE2) # Raw age
-
-str(surveydf$CATAGE) # Categorized Age
-table(surveydf$CATAGE) # Categorized Age
-
-# Respondent education
-str(surveydf$EDUCATN2) # Raw education
-table(surveydf$EDUCATN2) 
-
-str(surveydf$EDUCCAT2) # Categorized Education
-table(surveydf$EDUCCAT2) # Categorized Education
-
-# Respondenthealth status
-str(surveydf$HEALTH)
-table(surveydf$HEALTH)
-
-str(surveydf$HEALTH2) # Fair and poor health grouped together
-table(surveydf$HEALTH2)
 
 
 
